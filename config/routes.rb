4d529a061c1aa2697df010c795fa11b7e6d61888
nelson shoby <nelsonshoby009@gmail.com>
2021-09-23 21:26:18 +0530

    Added API based architecture

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@
 Rails.application.routes.draw do
 
   resources :tasks, only: :index
+  root "home#index"
+  get "*path", to: "home#index", via: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
