55de47bda067b1a84b31fa47d183928913ae1df0
nelson shoby <nelsonshoby009@gmail.com>
2021-10-08 12:48:31 +0530

    Implemented create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :tasks, only: %i[index create], param: :slug
 
-  resources :tasks, only: :index, param: :slug
   root "home#index"
   get "*path", to: "home#index", via: :all
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
