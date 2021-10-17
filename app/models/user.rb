06285431ccb132607303509d6fb8c1035338219e
nelson shoby <nelsonshoby009@gmail.com>
2021-10-18 00:08:13 +0530

    Added comments to tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
+  has_many :comments, dependent: :destroy
   has_secure_password
   has_secure_token :authentication_token
   before_destroy :assign_tasks_to_task_owners
