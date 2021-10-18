775f728b5178e171f5c6aeedb3553c3dca605917
nelson shoby <nelsonshoby009@gmail.com>
2021-10-18 13:11:54 +0530

    Added progress to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,21 +5,23 @@ class TasksController < ApplicationController
   after_action :verify_policy_scoped, only: :index
   before_action :authenticate_user_using_x_auth_token
   before_action :load_task, only: %i[show update destroy]
+  before_action :ensure_authorized_update_to_restricted_attrs, only: %i[update]
 
   def index
-    @tasks = policy_scope(Task)
-
-    render status: :ok, json: { tasks: tasks }
+    # tasks = policy_scope(Task)
+    tasks = policy_scope(Task)
+    @pending_tasks = tasks.pending.as_json(include: { assigned_user: { only: %i[name id] } })
+    @completed_tasks = tasks.completed
   end
 
   def create
     task = Task.new(task_params.merge(task_owner_id: @current_user.id))
     authorize task
-    if @task.save
+    if task.save
       render status: :ok,
         json: { notice: t("successfully_created", entity: "Task") }
     else
-      errors = @task.errors.full_messages.to_sentence
+      errors = task.errors.full_messages.to_sentence
       render status: :unprocessable_entity, json: { error: errors }
     end
   end
@@ -52,13 +54,21 @@ class TasksController < ApplicationController
   private
 
     def task_params
-      params.require(:task).permit(:title, :assigned_user_id)
+      params.require(:task).permit(:title, :assigned_user_id, :progress)
     end
 
     def load_task
       @task = Task.find_by(slug: params[:slug])
       unless @task
         render status: :not_found, json: { error: t("task.not_found") }
+      end
+    end
+
+    def ensure_authorized_update_to_restricted_attrs
+      is_editing_restricted_params = Task::RESTRICTED_ATTRIBUTES.any? { |a| task_params.key?(a) }
+      is_not_owner = @task.task_owner_id != @current_user.id
+      if is_editing_restricted_params && is_not_owner
+        handle_authorization_error
       end
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  RESTRICTED_ATTRIBUTES = %i[title user_id]
   belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
   belongs_to :task_owner, foreign_key: "task_owner_id", class_name: "User"
   has_many :comments, dependent: :destroy
+  enum progress: { pending: 0, completed: 1 }
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :slug, uniqueness: true
